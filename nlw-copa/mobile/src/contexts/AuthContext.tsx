cdae98a166cb8ce6c7ea5df39e9e5e08dcf856dc
Rafael Rodrigues <rafaelkunzler@gmail.com>
2022-11-04 20:29:29 -0300

    set envronment variables

--- nlw-copa/mobile/src/contexts/AuthContext.tsx
+++ nlw-copa/mobile/src/contexts/AuthContext.tsx
@@ -30,7 +30,7 @@ export function AuthContextProvider({ children }: AuthProviderProps) {
     const [isUserLoading, setIsUserLoading] = useState(false);
 
     const [request, response, promptAsync] = Google.useAuthRequest({
-        clientId: '417231044509-bg8c8n2pab4lrvrnt96dtvbt902s0lqh.apps.googleusercontent.com',
+        clientId: process.env.CLIENT_ID,
         redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
         scopes: ['profile', 'email']
     });
