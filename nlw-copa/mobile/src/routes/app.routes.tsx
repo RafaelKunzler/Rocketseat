de9b109c6e50a1f13fb51f0f71c7571177e75f43
Rafael Rodrigues <rafaelkunzler@gmail.com>
2022-11-04 17:58:44 -0300

    implement search option and started creating details page

--- nlw-copa/mobile/src/routes/app.routes.tsx
+++ nlw-copa/mobile/src/routes/app.routes.tsx
@@ -6,6 +6,7 @@ import { Platform } from 'react-native'
 import { New } from '../screens/New'
 import { Pools } from '../screens/Pools'
 import { Find } from '../screens/Find';
+import { Details } from '../screens/Details';
 
 const {Navigator, Screen} = createBottomTabNavigator();
 
@@ -57,6 +58,14 @@ export function AppRoutes(){
                     tabBarButton: () => null
                 }}
             />
+
+            <Screen 
+                name= "details"
+                component={Details}
+                options={{
+                    tabBarButton: () => null
+                }}
+            />  
         </Navigator>
     )
 }
--- nlw-copa/server/src/routes/pool.ts
+++ nlw-copa/server/src/routes/pool.ts
@@ -162,6 +162,7 @@ export async function poolRoutes(fastify: FastifyInstance) {
         const {id } = getPoolParams.parse(request.params)
     
         const pool = await prisma.pool.findUnique({
+            
             where: {
                 id,
             },
